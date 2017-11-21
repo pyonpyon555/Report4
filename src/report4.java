2f54405e6ac55e26d297175e1ab74b0225fd0801
Tomohiro Inoue <e175762@ie.u-ryukyu.ac.jp>
2017-11-21 14:23:02 +0900

    second commit report4

--- src/report4.java
+++ src/report4.java
@@ -1,6 +1,11 @@
 public class report4 {
     public static void main(String[] args){
         String str = null;
-        System.out.println(str.length());
+        try {
+            System.out.println(str.length());
+        } catch (NullPointerException e) {
+            System.out.println("例外が発生しました。");
+            System.out.println(e.getMessage());
+        }
     }
 }
