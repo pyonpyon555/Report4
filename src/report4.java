3fa1b427174a3d28a2e909f16a0520b01ba760c9
Tomohiro Inoue <e175762@ie.u-ryukyu.ac.jp>
2017-11-21 14:02:45 +0900

    first commit report4

--- src/report4.java
+++ src/report4.java
@@ -1,5 +1,5 @@
 public class report4 {
-    public static void main(String args[]){
+    public static void main(String[] args){
         String str = null;
         System.out.println(str.length());
     }
